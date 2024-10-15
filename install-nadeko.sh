e0ea673f22d1ea9b7050c68155102aefd7413496
RyuunosukeDS3 <31015675+RyuunosukeDS3@users.noreply.github.com>
2024-10-15 17:55:40 -0300

    Update install-nadeko.sh

--- install-nadeko.sh
+++ install-nadeko.sh
@@ -1,29 +1,6 @@
 #!/bin/sh
 
-echo "Installing dotnet"
-wget "https://packages.microsoft.com/config/ubuntu/$VER/packages-microsoft-prod.deb" -O packages-microsoft-prod.deb
-dpkg -i packages-microsoft-prod.deb
-rm packages-microsoft-prod.deb
-
-apt-get update;
-apt-get install -y apt-transport-https && apt-get update;
-apt-get install -y apt-transport-https && apt-get update;
-apt-get install -y dotnet-sdk-8.0;
-
-echo "Installing Git, Redis and Tmux..."
-apt-get install git tmux redis-server -y
-
-echo "Installing music prerequisites..."
-apt-get install libopus0 opus-tools libopus-dev libsodium-dev -y
-apt install python
-echo ""
-apt-get install ffmpeg
-wget https://yt-dl.org/downloads/latest/youtube-dl -O /usr/local/bin/youtube-dl
-chmod a+rx /usr/local/bin/youtube-dl
-
-echo "NadekoBot Prerequisites Installation completed..."
-
-git clone -b v5 --recursive --depth 1 https://gitlab.com/Kwoth/nadekobot
+git clone -b v4 --recursive --depth 1 https://gitlab.com/Kwoth/nadekobot
 
 root=$(pwd)
 
